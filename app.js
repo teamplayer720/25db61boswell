c82164bb609b6f7d88328bb1cdf640b03a79e4c6
Christian Boswell <97624302+teamplayer720@users.noreply.github.com>
2025-11-12 13:15:11 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,40 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+
+const connectionString = process.env.MONGO_CON;
+
+
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+var db = mongoose.connection;
+
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', function() {
+  console.log('Connection to MongoDB succeeded');
+});
+
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var artifactsRouter = require('./routes/artifacts'); 
 var gridRouter = require('./routes/grid');
 
-
-
 var app = express();
 
-// view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,24 +42,24 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/artifacts', artifactsRouter);
 app.use('/grid', gridRouter);
 
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+ 
   res.status(err.status || 500);
   res.render('error');
 });

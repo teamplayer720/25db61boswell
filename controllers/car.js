51e7d05172419ee4f8fa0a482f687581905cbc78
Christian Boswell <97624302+teamplayer720@users.noreply.github.com>
2025-11-17 11:09:51 -0600

    update

--- controllers/car.js
+++ controllers/car.js
@@ -131,7 +131,7 @@ exports.car_view_one_Page = async function(req, res) {
     }
 }; 
 
- //Handle building the view for creating a costume.
+//Handle building the view for creating a costume.
 // No body, no in path parameter, no query.
 // Does not need to be async
 exports.car_create_Page = function(req, res) {
